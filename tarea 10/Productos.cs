323552338589f0f35d1af2903c656f2eb7b23e75
gto26 <nxsmaritogtohernandez@gmail.com>
2024-08-06 14:40:49 -0600

    Agregar archivos de proyecto.

---
+++ tarea 10/Productos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tarea_10
+{
+    internal class Productos
+    {
+        public string ID  { get; set; }
+        public string Name { get; set; }
+        public double Price  { get; set; }
+
+        public Productos(string iD, string name, double price)
+        {
+            ID = iD;
+            Name = name;
+            Price = price;
+        }
+    }
+}
